e2c7dd85e7a4a5c3c504bc56b31c68ad357415b7
barros <mdbf42@gmail.com>
2022-09-16 19:09:16 -0300

    Fix @ Input Names

--- src/Adapters/ExtractRequest.ts
+++ src/Adapters/ExtractRequest.ts
@@ -5,11 +5,11 @@ export default class Request {
     static extractPartnerPlant(request: HttpRequest): PartnerPlant {
         const body = request.body;
         return new PartnerPlant({
-            userEmail: body['r6212ea4c87564c0ab60bff9921a93611'],
-            orderId: body['rcb7948e58ab643049ab9be48dabe3f43'],
-            partnerPassword: body['r6621d1c6613d4f87beaa8fc659fb4828'],
-            partnerCode: parseInt(body['rb1920c42d6ac479682e436f5863d0506'].split('-')[0]),
-            partnerName: body['rb1920c42d6ac479682e436f5863d0506'].split('-')[1]
+            userEmail: body.userEmail,
+            orderId: body.orderId,
+            partnerPassword: body.partnerPassword,
+            partnerCode: body.partner.split('-')[0],
+            partnerName: body.partner.split('-')[1]
         })
 
     }
